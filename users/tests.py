c7ee0fe3acee47fae6810d20e9feb7bcbfe69ff2
Ben Kelly <benelliottkelly@gmail.com>
2024-03-04 17:01:40 +0000

    added tests for users viewing their own profiles and other peoples profiles

--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,12 @@
 import json
-from django.contrib.auth.models import User
 from django.urls import reverse
-from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.test import APITestCase
 from rest_framework import status
-from users.serializers.common import RegistrationSerializer, UserSerializer
+from .models import User
 
+# As the DB is hosted on a free tier server, seperate test_DBs cannot be created due to permission issues
+# Therefore to run tests we need to use the existing DB using <python manage.py test --keepdb>
 class RegistrationTestCase(APITestCase):
 
   def test_registration(self):
@@ -21,3 +22,36 @@ class RegistrationTestCase(APITestCase):
     response = self.client.post('/api/auth/register/', data)
     # Test that 400 status is receieved
     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class UserViewSetTestCase(APITestCase):
+
+  def setUp(self):
+    # Because we are keeping the DB rather than creating a new test_DB we need to specify the pk, as there is already existing users and the PK will go up each time a test is run
+    self.user = User.objects.create_user(pk=999, username='testuser3', email='testuser3@email.com', password='another_unique_password')
+    self.token = RefreshToken.for_user(self.user)
+    self.api_authentication()
+
+  def api_authentication(self):
+    self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
+
+  # Test the endpoint for a single profile page while logged in
+  def test_user_detail_retrieve(self):
+    # Test the response for testuser3 looking at their own profile
+    response = self.client.get(reverse('user-detail', kwargs={'pk': 999}))
+    # Test the return of a 200 status code
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # Test the username retrieved is the one expected
+    self.assertEqual(response.data['username'], 'testuser3')
+  
+  # Test for user to update their own profile
+  def test_user_updated_by_owner(self):
+    response = self.client.patch(reverse('user-detail', kwargs={'pk': 999}), {'first_name': 'Testy', 'last_name': 'McTestFace'})    
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(json.loads(response.content), {'pk': 999, 'first_name': 'Testy', 'last_name': 'McTestFace', 'username': 'testuser3', 'email': 'testuser3@email.com', 'image': ''})
+
+  # Test for user trying to update someone else's profile
+  def test_user_updated_by_random_user(self):
+    random_user = User.objects.create_user(username='random', password='more_unique_passwords')
+    self.client.force_authenticate(user=random_user)
+    response = self.client.patch(reverse('user-detail', kwargs={'pk': 999}), {'first_name': 'Rude', 'last_name': 'Names'})
+    self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 urlpatterns = [
   path('register/', RegisterView.as_view()), # /api/auth/register/
   path('login/', TokenObtainPairView.as_view()), #/api/auth/login/
-  path('users/<int:pk>/', UserDetailView.as_view()) # /api/auth/users/:pk
+  path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'), # /api/auth/users/:pk
 ]
