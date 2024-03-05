4fb9890ab21dcd71bd7eb8db5208f6f051ba5d57
Ben Kelly <benelliottkelly@gmail.com>
2024-03-05 17:38:21 +0000

    adjusted dark_legion model to include subheading

--- dark_legion/models.py
+++ dark_legion/models.py
@@ -4,7 +4,8 @@ class DarkLegion(models.Model):
   card_number = models.PositiveIntegerField()
   name = models.CharField(max_length=255)
   image = models.CharField(max_length=550)
-  description = models.TextField(max_length=2000)
+  subheading = models.CharField(max_length=550, blank=True)
+  description = models.TextField(max_length=2000, blank=True)
 
   def __str__(self):
     return f'{self.card_number}: {self.name}'
